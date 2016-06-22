9d99f59d801d71af9ed9b5f78ee43704d2aca694
Anthony <martab7.am@gmail.com>
2016-06-22 14:27:34 -0500

    Updates simple simon project with all functionality

--- public/JS/simple_simon.js
+++ public/JS/simple_simon.js
@@ -1,40 +1,62 @@
-var boxOne = $('#square_one')
-var boxTwo = $('#square_two')
-var boxThree = $('#square_three')
-var boxFour = $('#square_four')
-var answerArray = []
+var simulatedPattern = []
+var randomlySelectedItem
+var randomBoxesArray = []
+var correct = true
+var index = 0
+var count
+var boxes = $('.box')
 
-var arrayOfSquares = [boxOne, boxTwo, boxThree, boxFour];
-var simulatedPattern = [];
+function start () {
+  randomlySelectedItem = boxes[Math.floor(Math.random() * boxes.length)]
+  simulatedPattern.push(randomlySelectedItem.getAttribute('data-value'))
+  animate();
+  // console.log(simulatedPattern)
+};
 
-var randomlySelectedItem;;
-
-function start() {
-  randomlySelectedItem = arrayOfSquares[Math.floor(Math.random()*arrayOfSquares.length)];
-  simulatedPattern.push(randomlySelectedItem);
-    simulatedPattern.forEach(function(){
-      randomlySelectedItem.animate({
-        'opacity': '.5'
-      }, 300, function(){
-        randomlySelectedItem.animate({
+function clickLights () {
+    boxes.each(function(){
+      $(this).click(function (){
+        $(this).animate({
+          'opacity': '.5'
+        }, 100).animate({
           'opacity': '1'
-        }, 300);
-      });
-      });
-    };
+        }, 100)
+      })
+  })
+}
+clickLights()
 
-$('.box').click(function(){
-  var answer = this.getAttribute('data-value');
-  console.log(answer);
-  answerArray.push(answer);
-  console.log(answerArray)
+function animate () {
+  simulatedPattern.forEach(function(randomlySelectedItem, index) {
+    setTimeout(function() {
+      $('[data-value="' + randomlySelectedItem + '"]').animate({
+        'opacity': '.5'
+      }, 500).animate({
+        'opacity': '1'
+      },500)
+    }, 500 * index)
+  })
+}
+
+$('.box').click(function(event){
+  if (simulatedPattern[index] === $(this).attr('data-value')) {
+    index++
+  } else {
+    simulatedPattern = []
+    correct = false
+    index = 0
+    randomBoxesArray = []
+  };
+  if (index === simulatedPattern.length && correct === true) {
+    start()
+    index = 0
+    count = simulatedPattern.length
+    $('span').text(count)
+    }
 })
-// $(i).click(function(){
-//
-// });
 
-$('#start').click(start);
-
-// setTimeout(function(){
-//   console.log('hello kids');
-// }, 3000);
+$('#start').click(function () {
+  simulatedPattern = []
+  $('span').text('1')
+  start()
+})
